c85590b24527702d5a1a07c747192f5f27ac1edb
SeaSailors <130479086+SeaSailors@users.noreply.github.com>
2025-04-21 14:02:15 +0800

    feat(compatv1): pass pruning options

--- compatv1/setup.go
+++ compatv1/setup.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"cosmossdk.io/log"
+	pruningtypes "cosmossdk.io/store/pruning/types"
 
 	dbm "github.com/cosmos/cosmos-db"
 	"github.com/cosmos/cosmos-sdk/baseapp"
@@ -22,21 +23,34 @@ func SetupStoreIAVL2(
 	baseAppOptions []func(*baseapp.BaseApp),
 	storeKeyNames []string,
 	appDBBackend dbm.BackendType,
+	pruningOptions pruningtypes.PruningOptions,
 ) []func(*baseapp.BaseApp) {
-	baseAppOptions = append([]func(*baseapp.BaseApp){setup(logger, db, homePath, storeKeyNames, appDBBackend)}, baseAppOptions...)
+	baseAppOptions = append([]func(*baseapp.BaseApp){
+		setup(logger, db, homePath, storeKeyNames, appDBBackend, pruningOptions),
+	}, baseAppOptions...)
 
 	return baseAppOptions
 }
 
-func setup(logger log.Logger, db dbm.DB, homePath string, storeKeyNames []string, appDBBackend dbm.BackendType) func(*baseapp.BaseApp) {
+func setup(
+	logger log.Logger,
+	db dbm.DB,
+	homePath string,
+	storeKeyNames []string,
+	appDBBackend dbm.BackendType,
+	pruningOptions pruningtypes.PruningOptions,
+) func(*baseapp.BaseApp) {
 	return func(bapp *baseapp.BaseApp) {
 		config := &root.Config{
 			Home:         homePath,
 			AppDBBackend: string(appDBBackend),
 			Options: root.Options{
-				SCType:          root.SCTypeIavlV2,
-				SCPruningOption: store.NewPruningOption(store.PruningNothing),
-				IavlV2Config:    iavlv2.DefaultOptions(),
+				SCType: root.SCTypeIavlV2,
+				SCPruningOption: store.NewPruningOptionWithCustom(
+					pruningOptions.KeepRecent,
+					pruningOptions.Interval,
+				),
+				IavlV2Config: iavlv2.DefaultOptions(),
 			},
 		}
 
